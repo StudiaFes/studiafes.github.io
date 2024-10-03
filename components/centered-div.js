9d19704d848fff6ec810ea532fda92015242dbab
StudiaFes <studiafes@gmail.com>
2024-10-03 17:30:28 +0200

    Using locally files instead of downloading via cdn

--- components/centered-div.js
+++ components/centered-div.js
@@ -7,12 +7,9 @@ class CenteredDiv extends HTMLElement {
     connectedCallback() {
         // Definire lo shadow DOM e inserire uno slot per il contenuto dinamico
         this.shadowRoot.innerHTML = `
-         <link rel="stylesheet" type="text/css" href="../../css/main.css">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-    crossorigin="anonymous"></script>
+         <link rel="stylesheet" type="text/css" href="/css/main.css">
+  <link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
+  <script src="js/bootstrap.bundle.min.js"></script>
                 <div class="container-fluid full-page d-flex align-items-center flex-column justify-content-center">
                     <slot></slot>
                 </div>
